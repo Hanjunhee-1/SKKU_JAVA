11aa05dbd484d6f3cf7d0bed667d150e2b89a479
Hanjunhee-1 <hanjuni857@naver.com>
2023-03-21 00:59:39 +0900

    ArrayListExample.java 작성

---
+++ 4주차/ArrayListExample.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayListExample {
+    public static void main(String[] args) {
+        List list = new ArrayList<>(Arrays.asList(1, 2, 3));
+
+        System.out.println(list);
+    }
+}
