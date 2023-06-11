18ee29ce5dda06148d58713cb72f5a727c3c3de2
Hanjunhee-1 <hanjuni857@naver.com>
2023-06-12 01:08:49 +0900

    main 수정

--- 14주차/UDP/UDPClient.java
+++ 14주차/UDP/UDPClient.java
@@ -23,7 +23,7 @@ public class UDPClient {
         datagramSocket.close();
     }
 
-    public void main(String args[]) {
+    public static void main(String args[]) {
         try {
             new UDPClient().start();
         } catch (Exception e) {
